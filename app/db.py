0e622b18994dd9436a972969b45ecca2c28c5e14
Philip Philip <philipandy2@gmail.com>
2021-02-15 11:51:20 +0100

    db crendentials

--- app/db.py
+++ app/db.py
@@ -9,7 +9,7 @@ database = 'postgres'
 pwd = 'postgres'
 port = 5432
 
-DB_URI = 'postgresql://{user}:{pwd}@{host}:{port}/{database}'
+DB_URI = f'postgresql://{user}:{pwd}@{host}:{port}/{database}'
 
 db_config = {
     'SQLALCHEMY_DATABASE_URI': DB_URI,
